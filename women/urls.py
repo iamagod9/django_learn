bd1bbf5792a35352f4a2771405582c1c5a89c79e
Заскин Александр Сергеевич <zaskin@KKST.KEM>
2023-05-12 14:46:22 +0700

    working with mixins

--- women/urls.py
+++ women/urls.py
@@ -4,9 +4,9 @@ from .views import *
 
 urlpatterns = [
     path('', WomenHome.as_view(), name='home'),
-    path('about/', about, name='about'),
+    path('about/', AboutPage.as_view(), name='about'),
     path('addpage/', AddPage.as_view(), name='add_page'),
-    path('contact/', contact, name='contact'),
+    path('contact/', Contact.as_view(), name='contact'),
     path('login/', login, name='login'),
     path('post/<slug:post_slug>/', ShowPost.as_view(), name='post'),
     path('category/<slug:cat_slug>/', WomenCategory.as_view(), name='category'),
--- women/views.py
+++ women/views.py
@@ -1,94 +1,71 @@
 from django.http import HttpResponse, Http404
 from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import *
 from .models import *
+from .utils import *
 
-# def index(request):
-#     posts = Women.objects.all()
-#
-#     context = {
-#         'posts': posts,
-#         'title': 'Main page',
-#         'cat_selected': 0,
-#     }
-#     return render(request, 'women/index.html', context=context)
-
-menu = [
-    {'title': 'О сайте', 'url_name': 'about'},
-    {'title': 'Добавить статью', 'url_name': 'add_page'},
-    {'title': 'Обратная связь', 'url_name': 'contact'},
-    {'title': 'Войти', 'url_name': 'login'},
-]
-class WomenHome(ListView):
+class WomenHome(DataMixin, ListView):
     model = Women
     template_name = 'women/index.html'
     context_object_name = 'posts'
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['menu'] = menu
-        context['title'] = 'Главная страница'
-        context['cat_selected'] = 0
-        return context
+        c_def = self.get_user_context(title='Главная страница')
+        return dict(list(context.items()) + list(c_def.items()))
 
     def get_queryset(self):
         return Women.objects.filter(is_published=True)
 
 
-def about(request):
-    return render(request, 'women/about.html', {'title': 'О сайте'})
-
-class AddPage(CreateView):
-    form_class = AddPostForm
-    template_name = 'women/addpage.html'
+class AboutPage(DataMixin, ListView):
+    model = Women
+    template_name = 'women/about.html'
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['menu'] = menu
-        context['title'] = 'Добавление статьи'
-        return context
+        c_def = self.get_user_context(title='О сайте')
+        return dict(list(context.items()) + list(c_def.items()))
 
-# def addpage(request):
-#     if request.method == 'POST':
-#         form = AddPostForm(request.POST, request.FILES )
-#         if form.is_valid():
-#                 form.save()
-#                 return redirect('home')
-#     else:
-#         form = AddPostForm()
-#     return render(request, 'women/addpage.html', {'form': form, 'title': 'Добавление статьи'})
+class AddPage(LoginRequiredMixin, DataMixin, CreateView):
+    form_class = AddPostForm
+    template_name = 'women/addpage.html'
+    success_url = reverse_lazy('home')
+    # login_url = reverse_lazy('home')
+    # raise_exception = True
 
-def contact(request):
-    return HttpResponse('Complete!')
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Добавление статьи')
+        return dict(list(context.items()) + list(c_def.items()))
+
+class Contact(DataMixin, ListView):
+    model = Women
+    template_name = 'women/contact.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Обратная связь')
+        return dict(list(context.items()) + list(c_def.items()))
 
 def login(request):
     return HttpResponse('Complete!')
 
-class ShowPost(DetailView):
+class ShowPost(DataMixin, DetailView):
     model = Women
     template_name = 'women/post.html'
     slug_url_kwarg = 'post_slug'
     context_object_name = 'post'
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = context['post']
-        context['menu'] = menu
-        return context
+        c_def = self.get_user_context(title=context['post'])
+        return dict(list(context.items()) + list(c_def.items()))
 
-# def show_post(request, post_slug):
-#     post = get_object_or_404(Women, slug=post_slug)
-#
-#     context = {
-#         'post': post,
-#         'title': post.title,
-#         'cat_selected': post.cat_id,
-#     }
-#
-#     return render(request, 'women/post.html', context=context)
-
-class WomenCategory(ListView):
+class WomenCategory(DataMixin, ListView):
     model = Women
     template_name = 'women/index.html'
     context_object_name = 'posts'
@@ -96,21 +73,9 @@ class WomenCategory(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Категория - ' + str(context['posts'][0].cat)
-        context['menu'] = menu
-        context['cat_selected'] = context['posts'][0].cat_id
-        return context
+        c_def = self.get_user_context(title='Категория - ' + str(context['posts'][0].cat),
+                                      cat_selected=context['posts'][0].cat_id)
+        return dict(list(context.items()) + list(c_def.items()))
 
     def get_queryset(self):
         return Women.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True)
-
-
-# def show_category(request, cat_slug):
-#     posts = Women.objects.filter(cat__slug=cat_slug)
-#
-#     context = {
-#         'posts': posts,
-#         'title': 'Отображение по рубрикам',
-#         'cat_selected': cat_slug,
-#     }
-#     return render(request, 'women/index.html', context=context)
