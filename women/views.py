590b6968e02d9cee308e360db5140f8eb1ed0d29
Заскин Александр Сергеевич <zaskin@KKST.KEM>
2023-05-12 16:54:15 +0700

    add authentication

--- women/views.py
+++ women/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import logout, login
+from django.contrib.auth.views import LoginView
 from django.http import HttpResponse, Http404
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
@@ -52,9 +54,6 @@ class Contact(DataMixin, ListView):
         c_def = self.get_user_context(title='Обратная связь')
         return dict(list(context.items()) + list(c_def.items()))
 
-def login(request):
-    return HttpResponse('Complete!')
-
 class ShowPost(DataMixin, DetailView):
     model = Women
     template_name = 'women/post.html'
@@ -90,3 +89,24 @@ class RegisterUser(DataMixin, CreateView):
         context = super().get_context_data(**kwargs)
         c_def = self.get_user_context(title='Регистрация')
         return dict(list(context.items()) + list(c_def.items()))
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('home')
+
+class LoginUser(DataMixin, LoginView):
+    form_class = LoginUserForm
+    template_name = 'women/login.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Авторизация')
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def get_success_url(self):
+        return reverse_lazy('add_page')
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
